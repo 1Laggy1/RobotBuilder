4b2e560ab75edebd4aa0cde5c8bdeff697bc3e0f
Andryushka <andryushka05@gmail.com>
2026-06-05 19:14:55 +0300

    Robot
    Changed Robot class:
    
    - Added spawnedParts dictionary.
    - Added defaultParts to default parts to robot.
    - RespawnPart function with two versions one that getting RobotPart_SO[]
      and respawning all parts, this will also check if default parts
      doesn't contain parts with the same part types. And second version is
      for respawning one robot part.
    - TorsoAddOffset function used to set positions of FromTorsoPart's to
      correct one for this torso and to add offset of each part.
    
    Changed TorsoPart:
    
    - Added public struct PartSocketMapping so you can add as many part
      position as you want.
    - Added InitializeDictionary that will create dictionary of
      PartSocketMapping for easier use in code.
    - Added AddOffsetToPart that will correct positions of all
      FromTorsoParts and also calling AddOffset function in part.

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -1,5 +1,56 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Robot : MonoBehaviour
 {
+    private Dictionary<PartType, GameObject> spawnedParts = new Dictionary<PartType, GameObject>();
+    [SerializeField] 
+    RobotPart_SO[] defaultParts;
+
+    void Start()
+    {
+        RespawnPart(defaultParts);
+    }
+
+    void RespawnPart(RobotPart_SO[] part)
+    {
+        if (spawnedParts.Count > 0)
+        {
+            foreach(var spawnedPart in spawnedParts)
+            {
+                Destroy(spawnedPart.Value);
+            }
+            spawnedParts.Clear();
+        }
+        
+        foreach(RobotPart_SO part_SO in part)
+        {
+            if (spawnedParts.ContainsKey(part_SO.partType))
+            {
+                Debug.LogError("Only one of each part type can be in the robot");
+                return;
+            }
+            GameObject spawnedPart = Instantiate(part_SO.prefab, this.transform);
+            spawnedParts.Add(part_SO.partType, spawnedPart);
+        }
+        TorsoAddOffset();
+    }
+    public void RespawnPart(RobotPart_SO part)
+    {
+        Destroy(spawnedParts[part.partType]);
+        Instantiate(part.prefab, this.transform);
+        TorsoAddOffset();
+    }
+    void TorsoAddOffset()
+    {
+        TorsoPart torsoPart = spawnedParts[PartType.Torso].GetComponent<TorsoPart>();
+        foreach(var part in spawnedParts)
+        {
+            FromTorsoPart fromTorsoPart = part.Value.GetComponent<FromTorsoPart>();
+            if (fromTorsoPart)
+            {
+                torsoPart.AddOffsetToPart(fromTorsoPart);
+            }
+        }
+    }
 }
--- Assets/Scripts/TorsoPart.cs
+++ Assets/Scripts/TorsoPart.cs
@@ -1,10 +1,41 @@
+using System.Collections.Generic;
 using UnityEngine;
+[System.Serializable]
+public struct PartSocketMapping
+{
+    public PartType partType;
+    public Transform socketTransform;
+}
 
 public class TorsoPart : RobotPart
 {
     [Header("Sockets for other parts")]
     [SerializeField] 
-    public Transform headSocket;
-    [SerializeField] 
-    public Transform legsSocket;
+    PartSocketMapping[] socketMappings;
+    Dictionary<PartType, Transform> socketDictionary;
+    bool isDictionary = false;
+    public void AddOffsetToPart(FromTorsoPart part)
+    {
+        if (!isDictionary)
+        {
+            InitializeDictionary();
+        }
+        if (socketDictionary.ContainsKey(part.robotPart_SO.partType))
+        {
+            part.AddOffset(socketDictionary[part.robotPart_SO.partType].position);
+        }
+    }
+    void InitializeDictionary()
+    {
+        isDictionary = true;
+        socketDictionary = new Dictionary<PartType, Transform>();
+
+        foreach (var mapping in socketMappings)
+        {
+            if (!socketDictionary.ContainsKey(mapping.partType))
+            {
+                socketDictionary.Add(mapping.partType, mapping.socketTransform);
+            }
+        }
+    }
 }
